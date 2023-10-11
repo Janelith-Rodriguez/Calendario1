0d05de0e193e6bb8fd83420bf8c891f5922de4d0
Usuario <Usuario@DESKTOP-5N0MR6R>
2023-10-11 02:21:58 -0300

    Agregar archivos de proyecto.

---
+++ Calendario1.FE/FrmCalendario.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace Calendario1.FE
+{
+    partial class FrmCalendario
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Hora = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Actividades = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.CalendarDimensions = new System.Drawing.Size(5, 2);
+            this.monthCalendar1.Location = new System.Drawing.Point(18, 2);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 0;
+            this.monthCalendar1.DateChanged += new System.Windows.Forms.DateRangeEventHandler(this.monthCalendar1_DateChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 340);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Hora,
+            this.Actividades});
+            this.dataGridView1.Location = new System.Drawing.Point(40, 376);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(503, 117);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellEndEdit);
+            // 
+            // Hora
+            // 
+            this.Hora.HeaderText = "Hora";
+            this.Hora.Name = "Hora";
+            this.Hora.ReadOnly = true;
+            // 
+            // Actividades
+            // 
+            this.Actividades.HeaderText = "Actividades";
+            this.Actividades.Name = "Actividades";
+            this.Actividades.Width = 500;
+            // 
+            // FrmCalendario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1042, 505);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.monthCalendar1);
+            this.Name = "FrmCalendario";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.FrmCalendario_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Hora;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Actividades;
+    }
+}
+
---
+++ Calendario1.FE/FrmCalendario.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calendario1.FE
+{
+    public partial class FrmCalendario : Form
+    {
+        public FrmCalendario()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmCalendario_Load(object sender, EventArgs e)
+        {
+            for(int f= 1; f <=96; f++)
+            {
+                dataGridView1.Rows.Add();
+            }
+            CargarFecha();
+        }
+
+        private void CargarFecha()
+        {
+            DateTime select = monthCalendar1.SelectionStart;
+            label1.Text="Fecha seleccionada:"+select.ToString("dd/MM/yyyy");
+            string fecha=select.Year.ToString()+select.Month.ToString()+select.Day.ToString();
+            if(!File.Exists(fecha) ) 
+            {
+                StreamWriter archivo = new StreamWriter(fecha);
+                DateTime fe = DateTime.Today;
+                for(int f=1; f <=96;f++)
+                {
+                    archivo.WriteLine(fe.ToString("HH:mm"));
+                    archivo.WriteLine("");
+                    fe = fe.AddMinutes(15);
+                }
+                archivo.Close();
+            }
+            StreamReader archivo2 = new StreamReader(fecha);
+            int x = 0;
+            while(!archivo2.EndOfStream) 
+            {
+                string linea1 = archivo2.ReadLine();
+                string linea2 = archivo2.ReadLine();
+                dataGridView1.Rows[x].Cells[0].Value = linea1;
+                dataGridView1.Rows[x].Cells[1].Value = linea2;
+                x++;
+            }
+            archivo2.Close();
+        }
+
+        private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
+        {
+            CargarFecha();
+        }
+
+        private void dataGridView1_CellEndEdit(object sender, DataGridViewCellEventArgs e)
+        {
+            DateTime select = monthCalendar1.SelectionStart;
+            string fecha = select.Year.ToString() + select.Month.ToString() + select.Day.ToString();
+            StreamWriter archivo = new StreamWriter(fecha);
+            for (int f=0;f<dataGridView1.Rows.Count; f++) 
+            {
+                archivo.WriteLine(dataGridView1.Rows[f].Cells[0].Value.ToString());
+                if (dataGridView1.Rows[f].Cells[1].Value != null)
+                    archivo.WriteLine(dataGridView1.Rows[f].Cells[1].Value.ToString());
+                else
+                    archivo.WriteLine("");
+            }
+            archivo.Close();
+        }
+    }
+}
